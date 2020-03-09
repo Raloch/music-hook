890785dad65e2c40e02b8a6745426a461d1cefe9
Ryan <891601405@qq.com>
2020-03-09 23:13:29 +0800

    '音乐app'

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IconStyle } from './assets/iconfont/iconfont';
+import { GlobalStyle } from './style';
+import { renderRoutes } from 'react-router-config';
+import routes from './routes/index.js';
+import { HashRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <HashRouter>
+      <GlobalStyle />
+      <IconStyle />
+      {renderRoutes(routes)}
+    </HashRouter>
+  );
+}
+
+export default App;
